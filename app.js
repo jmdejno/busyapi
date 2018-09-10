cfdb275df520ded642a13d2d822d49e9a9ad020a
Jeremy Dejno <jeremy.dejno@gmail.com>
2018-09-10 12:21:47 -0500

    Remove static file serving, non-essential routes, and view engine.

--- app.js
+++ app.js
@@ -1,33 +1,14 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+require('dotenv').config();
 
 var app = express();
 
-// simple in-memory usage store
-var usages = [];
-app.usages = usages;
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
 
 // API
 require('./routes/api/usages')(app);
@@ -47,7 +28,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json(err);
 });
 
 module.exports = app;
