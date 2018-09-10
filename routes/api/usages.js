4b866018bdde5a42692ac75a27aae666dfd94e25
Jeremy Dejno <jeremy.dejno@gmail.com>
2018-09-10 12:27:01 -0500

    Connect redis client, push POST data into redis list and return list length as usageId.

--- routes/api/usages.js
+++ routes/api/usages.js
@@ -1,12 +1,39 @@
-module.exports = function(app){
-    app.post('/api/usages', function(req, res){
+const redis = require('redis');
 
-        // Store the supplied usage data
-        app.usages.push(req.body);
+//set REDIS config options (local vs. remote)
+const REDIS_HOST = process.env.NODE_ENV === 'development' ? null : process.env.REDIS_MASTER_HOST;
+const REDIS_PASSWORD = process.env.NODE_ENV === 'development' ? undefined : process.env.REDIS_PASSWORD;
 
-        var usageId = app.usages.length;
-        console.log('Stored usage count: ' + usageId);
+//connect to REDIS
+const client = redis.createClient({
+  host: REDIS_HOST,
+  password: REDIS_PASSWORD
+});
 
-        res.status(201).json({'id':usageId});
+
+client.on('connect', () => console.log(`Redis connected: ${REDIS_HOST || 'localhost'}.`));
+client.on('error', err => console.error(err));
+
+module.exports = function (app)
+{
+  /**
+   * POST medication usage for patient.
+   */
+  app.post('/api/usages', function (req, res)
+  {
+    // TODO: handle error here with appropriate 4xx code
+
+    // define user key and data
+    const key = 'user:' + req.body.patientId + ':usage';
+    const usageData = {timestamp: req.body.timestamp, medication: req.body.medication};
+
+    // push data onto list
+    client.lpush(key, JSON.stringify(usageData), (err, usageId) => {
+      // TODO: handle redis error here (4xx or 5xx codes based on err)
+      res.status(201).json({'id': usageId});
     });
-}
+
+  });
+
+};
+
