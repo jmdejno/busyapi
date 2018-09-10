b75a189c9e184341bc9941d3274fa2f22feca715
Jeremy Dejno <jeremy.dejno@gmail.com>
2018-09-10 12:35:53 -0500

    Add development configuration to pm2 process file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,13 @@ module.exports = {
     script: "./bin/www",
     instances: os.cpus().length - 1, // due to redis and ApacheBench running locally, leaving one core open.
     exec_mode: "cluster",
+    env: {
+      PORT: 3000,
+      NODE_ENV: 'development'
+    },
     env_production: {
-	PORT: 8080,
-	NODE_ENV: "production"
+	    PORT: 8080,
+	    NODE_ENV: "production"
    }
   }]
 };
